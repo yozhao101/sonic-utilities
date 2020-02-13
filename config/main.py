0cab2877fa79400a2ea5b0c307d9fcd418dcf9d1
Yong Zhao <yozhao@microsoft.com>
2020-02-13 11:55:26 -0800

    [config/main.py] Use constant string to represent the path of init_cfg.json.
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

--- config/main.py
+++ config/main.py
@@ -546,7 +546,7 @@ def reload(filename, yes, load_sysinfo):
         command = "{} -H -k {} --write-to-db".format(SONIC_CFGGEN_PATH, cfg_hwsku)
         run_command(command, display_cmd=True)
 
-    if os.path.isfile('/etc/sonic/init_cfg.json'):
+    if os.path.isfile(INIT_CFG_FILE):
         command = "{} -j {} -j {} --write-to-db".format(SONIC_CFGGEN_PATH, INIT_CFG_FILE, filename)
     else:
         command = "{} -j {} --write-to-db".format(SONIC_CFGGEN_PATH, filename)
